c9a27b6934ccd5453deda7bcb5f215b33d6bc51e
RakibMatubbar <rakibmatubbar996@gmail.com>
2026-02-21 19:00:06 +0600

    / Problem

--- script/login.js
+++ script/login.js
@@ -21,7 +21,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
         alert('Login Success.');
 
         // window.location.replace('/home.html')
-        window.location.assign('/home.html')
+        window.location.assign('home.html')
     }
     else {
         // console.log('Login Failed.');
